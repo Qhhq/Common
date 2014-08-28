c11be217ca5a61a526e9ab33b71eaec649119724
Qhhq <anya_alt@mail.ru>
2014-08-29 00:05:26 +0400

    utf_to_utf

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 ﻿#include <fstream>
-#include <codecvt>
 
-// convert wstring to UTF-8 string
-std::string wstring_to_utf8(const std::wstring& str)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
-    return myconv.to_bytes(str);
-}
+#define BOOST_ALL_DYN_LINK
+#include <boost/locale.hpp>
+
+#define NAME L"Тест"
 
 int main(int argc, char *argv[])
 {
-    std::ofstream f("test.csv", std::ios::out | std::ios::binary);
-    unsigned char marker[] = { 0xEF, 0xBB, 0xBF, 0 };
-    f << marker;
-    f << wstring_to_utf8(L"Тест");
-
-    return 0;
+	std::ofstream f("c:/123\\test.csv", std::ios::out | std::ios::binary);
+	unsigned char marker[] = { 0xEF, 0xBB, 0xBF, 0 };
+	f << marker;
+	f << boost::locale::conv::utf_to_utf<char>(NAME);
+	f << boost::locale::conv::utf_to_utf<char>("test");
+	return 0;
 }
